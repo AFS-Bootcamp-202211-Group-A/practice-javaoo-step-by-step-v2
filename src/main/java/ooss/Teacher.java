1cffc5cf7348598b043b0ffe62b47be16452ad93
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-11-30 17:41:19 +0800

    feat: Implemented methods for Teacher, including assignTo, belongsTo and isTeaching

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,12 +1,34 @@
 package ooss;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Teacher extends Person{
+
+    private ArrayList<Klass> responsibleClasses = new ArrayList<>();
 
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
 
+    public void assignTo(Klass klass){
+        responsibleClasses.add(klass);
+    }
+
+    public boolean belongsTo(Klass klass){
+        return responsibleClasses.contains(klass);
+    }
+
+    public boolean isTeaching(Student student){
+        return responsibleClasses.stream().anyMatch(student::isIn);
+    }
+
+
     public String introduce() {
-        return super.introduce() + " I am a teacher.";
+
+        List<String> responsibleClassesString = responsibleClasses.stream().map(Klass::getNumber).map(Object::toString).collect(Collectors.toList());
+        String classes = String.join(", ", responsibleClassesString);
+        return super.introduce() + " I am a teacher." + " I teach Class " + classes + ".";
     }
 }
