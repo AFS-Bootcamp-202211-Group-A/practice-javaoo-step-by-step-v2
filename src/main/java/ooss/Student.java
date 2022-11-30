2618ecc0f285b7d66582778b6447474455ea3cda
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-11-30 17:40:14 +0800

    feat: (step3) implement Student with kClass attr, join method, isIn method.

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -4,12 +4,27 @@ import java.text.MessageFormat;
 
 public class Student extends Person{
 
+    private Klass klass;
+
     public Student(int id, String name, int age){
         super(id, name, age);
     }
 
     @Override
     public String introduce() {
-        return MessageFormat.format("{0} I am a student.",super.introduce());
+        String introduction = MessageFormat.format("{0} I am a student.", super.introduce());
+        if (klass != null) {
+            introduction = MessageFormat.format("{0} I am in class {1}.", introduction, klass.getNumber());
+        }
+        return introduction;
+    }
+
+    public void join(Klass klass){
+        this.klass = klass;
+    }
+
+    public boolean isIn(Klass klass){
+        if(this.klass == null) return false;
+        return this.klass.equals(klass);
     }
 }
