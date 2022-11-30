1728b5a51cac2cc46aa718497d37803eb536b90e
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-11-30 17:07:21 +0800

    feat: Implemented Person with override equals comparison

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -1,4 +1,30 @@
 package ooss;
 
 public class Person {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    public Person(int id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (getClass() != obj.getClass())
+            return false;
+        Person anotherPerson = (Person) obj;
+        return anotherPerson.id == this.id;
+    }
+
+
+
 }
