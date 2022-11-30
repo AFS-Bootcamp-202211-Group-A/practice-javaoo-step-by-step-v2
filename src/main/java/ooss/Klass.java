5acd2412d2af8580274bb4b63cab7da4e0340fae
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-11-30 17:23:27 +0800

    feat: Implemented class Klass and join & isIN method for Student which now has a Klass variable

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -1,4 +1,24 @@
 package ooss;
 
 public class Klass {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+        if (!(obj instanceof Klass)) return false;
+
+        Klass anotherKlass = (Klass) obj;
+        return anotherKlass.number == this.number;
+    }
+
 }
--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -2,13 +2,22 @@ package ooss;
 
 public class Student extends Person{
 
+    private Klass klass;
+
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
     public String introduce() {
-        return super.introduce() + " I am a student.";
+        return super.introduce() + " I am a student." + " I am in class " + klass.getNumber()+ ".";
     }
 
+    public void join(Klass newKlass){
+        this.klass = newKlass;
+    }
+
+    public boolean isIn(Klass klass){
+        return this.klass == klass;
+    }
 
 }
