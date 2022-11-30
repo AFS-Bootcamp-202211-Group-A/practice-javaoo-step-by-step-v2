384f372c87fbd5f66f9003ce8d41f08ae8b0d3d6
kwokth4 <kwokth4@oocl.com>
2022-11-30 17:39:28 +0800

    Step 3 Done

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,11 +1,24 @@
 package ooss;
 
 public class Student extends Person {
+    private int klass;
     public Student(int id, String name, int age) {
         super(id,name,age);
     }
     public String introduce(){
-        return "My name is "+super.name+". I am "+super.age+" years old. "
+        String msg = "My name is "+super.name+". I am "+super.age+" years old. "
                 + "I am a student.";
+        if(klass != 0){
+            msg += " I am in class "+klass+".";
+        }
+        return msg;
+    }
+
+    public boolean isIn(Klass klass) {
+        return this.klass == klass.getNumber();
+    }
+
+    public void join(Klass klass) {
+        this.klass = klass.getNumber();
     }
 }
