d10641e734ff6a5d8ec71f50c019c5346664f35d
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-11-30 17:22:32 +0800

    feat:(step2) implement student with introduce method

--- src/test/java/ooss/step2/PersonTest.java
+++ src/test/java/ooss/step2/PersonTest.java
@@ -9,14 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonTest {
 
-//    @Test
-//    public void should_return_message_with_name_and_age_when_introduce_given_student() {
-//        Person tom = new Student(1, "Tom", 18);
-//        String introduce = tom.introduce();
-//
-//        assertEquals("My name is Tom. I am 18 years old. I am a student.", introduce);
-//    }
-//
+    @Test
+    public void should_return_message_with_name_and_age_when_introduce_given_student() {
+        Person tom = new Student(1, "Tom", 18);
+        String introduce = tom.introduce();
+
+        assertEquals("My name is Tom. I am 18 years old. I am a student.", introduce);
+    }
+
 //    @Test
 //    public void should_return_message_with_name_and_age_when_introduce_given_teacher() {
 //        Person jerry = new Teacher(1, "Jerry", 21);
